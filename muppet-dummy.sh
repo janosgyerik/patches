cc0999afc54e634d786e4ed77857771ec7414fea
Janos Gyerik <janos.gyerik@sonarsource.com>
2019-08-08 19:57:51 +0200

    Add sonar.muppet.url property to muppet dummy

--- muppet-dummy.sh
+++ muppet-dummy.sh
@@ -4,4 +4,11 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-set_property muppet.secret.token foo $SQ_HOME/conf/sonar.properties
+setp() {
+    local name=$1; shift
+    local value=$1; shift
+    set_property "$name" "$value" "$SQ_HOME/conf/sonar.properties"
+}
+
+setp muppet.secret.token foo
+setp sonar.muppet.url "http://127.0.0.1:9876"
