69d04d65794f7c173d06c250f36441f3cf20d34a
Janos Gyerik <janos.gyerik@sonarsource.com>
2019-08-08 20:07:00 +0200

    Add Q property for sonarcloud

--- sonarcloud.sh
+++ sonarcloud.sh
@@ -4,4 +4,16 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-set_property sonar.sonarcloud.enabled true $SQ_HOME/conf/sonar.properties
+setp() {
+    local name=$1; shift
+    local value=$1; shift
+    set_property "$name" "$value" "$SQ_HOME/conf/sonar.properties"
+}
+
+prop_name=sonar.cloud.shared.EventsCoreQueueUri
+prop_value=https://sqs.$AWS_REGION.amazonaws.com/$AWS_NNN/Core-Events-$ENV_NAME-Q
+setp "$prop_name" "$prop_value"
+
+prop_name=sonar.cloud.autoscan.authSecret
+prop_value=$SC_AUTOSCAN_CORE_SECRET
+setp "$prop_name" "$prop_value"
