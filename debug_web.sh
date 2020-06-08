b398c5386831b79926682c76721077a11255c188
Janos Gyerik <janos.gyerik@sonarsource.com>
2020-06-08 16:49:09 +0200

    Use the same debug port for web as the rest of the team

--- debug_web.sh
+++ debug_web.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 source scripts/property_utils.sh
 
 SQ_HOME=$1
+port=5005
 
-echo "enabling debug on web server, listening on port 5006"
-set_property sonar.web.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006 $SQ_HOME/conf/sonar.properties
+echo "enabling debug on web server, listening on port $port"
+set_property sonar.web.javaAdditionalOpts "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$port" $SQ_HOME/conf/sonar.properties
