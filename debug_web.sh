b94a721530a807e0022bfabb4a343b69083ca8ec
Janos Gyerik <janos.gyerik@sonarsource.com>
2021-05-03 12:26:14 +0200

    Add suspend parameter for debug_web

--- debug_web.sh
+++ debug_web.sh
@@ -4,6 +4,13 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 port=5005
+suspend=n
+
+setp() {
+    local name=$1; shift
+    local value=$1; shift
+    set_property "$name" "$value" "$SQ_HOME/conf/sonar.properties"
+}
 
 echo "enabling debug on web server, listening on port $port"
-set_property sonar.web.javaAdditionalOpts "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$port" $SQ_HOME/conf/sonar.properties
+setp sonar.web.javaAdditionalOpts "-agentlib:jdwp=transport=dt_socket,server=y,suspend=$suspend,address=$port"
